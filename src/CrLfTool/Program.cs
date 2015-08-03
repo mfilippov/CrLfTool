26a192e4435555f74b94e7c9a3890bd7b432f5a0
Mikhail Filippov <mikhail@filippov.me>
2015-08-03 12:45:46 +0300

    Add support extension list in config.

---
+++ src/CrLfTool/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CrLfTool
+{
+  class Program
+  {
+    private static Regex UnixLineEndingRx = new Regex("([^\r])\n", RegexOptions.Compiled);
+    private static Regex WindowsLineEndingRx = new Regex("\r\n", RegexOptions.Compiled);
+
+    static void Main(string[] args)
+    {
+      MainAsync(args).Wait();
+    }
+
+    private async static Task MainAsync(string[] args)
+    {
+      var result = true;
+      if ((args.Length != 3) || (args[0] != "fix" && args[0] != "validate") || (args[1] != "unix" && args[1] != "windows"))
+      {
+        Console.WriteLine("Usage: CrLfTool fix|validate unix|windows path");
+        return;
+      }
+      else if (!Directory.Exists(args[2]))
+      {
+        Console.WriteLine("Path should be valid directory");
+        return;
+      }
+      else
+      {
+        if (!await ProcessDirectory(new DirectoryInfo(args[2]), args[0] == "fix" ? ActionType.Fix : ActionType.Validate, args[1] == "unix" ? LineEnding.Unix : LineEnding.Windows))
+        {
+          result = false;
+        }
+      }
+      if (!result)
+      {
+        throw new InvalidDataException("Validation error");
+      }
+    }
+    private static async Task<bool> ProcessDirectory(DirectoryInfo dirInfo, ActionType actionType, LineEnding lineEnding)
+    {
+      bool result = true;
+      if (!dirInfo.Attributes.HasFlag(FileAttributes.ReparsePoint))
+      {
+        foreach (var d in dirInfo.GetDirectories())
+        {
+          if (!await ProcessDirectory(d, actionType, lineEnding))
+          {
+            result = false;
+          }
+        }
+        foreach (var f in dirInfo.GetFiles())
+        {
+          if (!await ProcessFile(f, actionType, lineEnding))
+          {
+            result = false;
+          }
+        }
+      }
+      return result;
+    }
+
+    private static async Task<bool> ProcessFile(FileInfo fileInfo, ActionType actionType, LineEnding lineEnding)
+    {
+      if (!fileInfo.Attributes.HasFlag(FileAttributes.ReparsePoint) && fileInfo.Extension.IsValidExtensionForProcessing())
+      {
+        if (actionType == ActionType.Fix)
+        {
+          return await FixFile(fileInfo, lineEnding);
+        }
+        else if (actionType == ActionType.Validate)
+        {
+          return await ValidateFile(fileInfo, lineEnding);
+        }
+      }
+      return true;
+    }
+
+    private static async Task<bool> FixFile(FileInfo fileInfo, LineEnding lineEnding)
+    {
+      string content;
+      using (var rdr = File.OpenText(fileInfo.FullName))
+      {
+        content = await rdr.ReadToEndAsync();
+        rdr.Close();
+      }
+      if (lineEnding == LineEnding.Unix)
+      {
+        content = WindowsLineEndingRx.Replace(content, "\n");
+      }
+      if (lineEnding == LineEnding.Windows)
+      {
+        content = UnixLineEndingRx.Replace(content, "$1\r\n");
+      }
+      using (var wrt = new StreamWriter(fileInfo.FullName))
+      {
+        await wrt.WriteAsync(content);
+        wrt.Close();
+      }
+      return true;
+    }
+
+    private static async Task<bool> ValidateFile(FileInfo fileInfo, LineEnding lineEnding)
+    {
+      using (var rdr = File.OpenText(fileInfo.FullName))
+      {
+        var content = await rdr.ReadToEndAsync();
+        rdr.Close();
+        if (lineEnding == LineEnding.Unix)
+        {
+          if (WindowsLineEndingRx.Match(content).Success)
+          {
+            Console.WriteLine($"Invalid line ending in file: {fileInfo.FullName}");
+            return false;
+          }
+        }
+        else if (lineEnding == LineEnding.Windows)
+        {
+          if (UnixLineEndingRx.Match(content).Success)
+          {
+            Console.WriteLine($"Invalid line ending in file: {fileInfo.FullName}");
+            return false;
+          }
+        }
+      }
+      return true;
+    }
+  }
+
+  internal enum ActionType
+  {
+    Fix,
+    Validate
+  }
+
+  internal enum LineEnding
+  {
+    Unix,
+    Windows
+  }
+
+  internal static class StringExtensions
+  {
+    public static bool IsValidExtensionForProcessing(this string extension)
+    {
+      var extensionList = string.IsNullOrEmpty(ConfigurationManager.AppSettings["ExtensionList"])
+        ? new List<string> { ".cs", ".cshtml", ".txt", ".js", ".xml" }
+        : new List<string>(ConfigurationManager.AppSettings["ExtensionList"].Split(';'));
+      return extensionList.Contains(extension);
+    }
+  }
+}
---
+++ src/CrLfTool/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CrLfTool")]
+[assembly: AssemblyDescription("Tool for fix and validation CRLF problem.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mikhail Filippov")]
+[assembly: AssemblyProduct("CrLfTool")]
+[assembly: AssemblyCopyright("Copyright © Mikhail Filippov 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f92c62c5-ef7c-4c34-aa72-619e01417db6")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
